c9aa02760f9fdcd095b6019001753ecdc86cbcf1
yvetteveve <yvetteumubyeyi2@gmail.com>
2018-12-21 19:31:57 +0200

    delete spacing in java.js

--- java.js
+++ java.js
@@ -1,64 +1,83 @@
 var count = 0;
-function check() {
-   
-
-  if (questionOne == "2") {
-    count += 5;
-  }
-  if (questionTwo == "2") {
-    count += 5;
-  }
-  if (questionThree == "3") {
-    count += 5;
-  }
-  if (questionFour == "3") {
-    count += 5;
-  }
-  if (questionFive == "4") {
-    count += 5;
-  }
-  if (questionSix == "2") {
-    count += 5;
-  }
-  if (questionSeven == "2") {
-    count += 5;
-  }
-  if (questionEight == "3") {
-    count += 5;
-  }
-  if (questionNine == "2") {
-    count += 5;
-  }
-  if (questionTen == "4") {
-    count += 5;
-  }
-  function displayer(){
-  document.getElementById("after_submit").style.visibility = "visible";
-
-  document.getElementById("number_count").innerHTML =
-    "You got " + count + "/50";
-  }
- 
+function check(
+ questionOne,
+ questionTwo,
+ questionThree,
+ questionFour,
+ questionFive,
+ questionSix,
+ questionSeven,
+ questionEight,
+ questionNine,
+ questionNine,
+ questionTen
+) {
+ if (questionOne == "2") {
+   count += 2;
+ }
+ if (questionTwo == "1") {
+   count += 2;
+ }
+ if (questionThree == "3") {
+   count += 2;
+ }
+ if (questionFour == "1") {
+   count += 2;
+ }
+ if (questionFive == "2") {
+   count += 2;
+ }
+ if (questionSix == "2") {
+   count += 2;
+ }
+ if (questionSeven == "4") {
+   count += 2;
+ }
+ if (questionEight == "4") {
+   count += 2;
+ }
+ if (questionNine == "1") {
+   count += 2;
+ }
+ if (questionTen == "3") {
+   count += 2;
+ }
+ return count;
 }
-$(document).ready(function(){
-  $("form#form").submit(function(event){
-  event.preventDefault();
-  var questionOne =$("input:radio[name:first]:checked");
-  var questionTwo = $("input:radio[name:second]:checked");
-  var questionThree = $("input:radio[name:third]:checked");
-  var questionFour = $("input:radio[name:fourth]:checked");
-  var questionFive = $("input:radio[name:fifth]:checked");
-  var questionSix = $("input:radio[name:sixth]:checked");
-  var questionSeven = $("input:radio[name:seventh]:checked");
-  var questionEight = $("input:radio[name:eighth]:checked");
-  var questionNine = $("input:radio[name:ninth]:checked");
-  var questionTen = $("input:radio[name:tenth]:checked");
+$(document).ready(function() {
+ $("#radio form").submit(function() {
+   var Question1 = $("input[type='radio'][name='first']:checked").val();
+   var Question2 = $("input[type='radio'][name='second']:checked").val();
+   var Question3 = $("input[type='radio'][name='third']:checked").val();
+   var Question4 = $("input[type='radio'][name='fourth']:checked").val();
+   var Question5 = $("input[type='radio'][name='fifth']:checked").val();
+   var Question6 = $("input[type='radio'][name='sixth']:checked").val();
+   var Question7 = $("input[type='radio'][name='seventh']:checked").val();
+   var Question8 = $("input[type='radio'][name='eighth']:checked").val();
+   var Question9 = $("input[type='radio'][name='ninth']:checked").val();
+   var Question10 = $("input[type='radio'][name='tenth']:checked").val();
 
-  $("#button").hide();
-  $("#quiz").hide();
-  $("#number_count").show();
-  displayer();
-  event.preventDefault();
-    
-  })
-})
+   console.log(Question1)
+
+   $("#button").hide();
+   $("#quiz").hide();
+   $("#number_count").text(
+     "You got " +
+       check(
+         Question1,
+         Question2,
+         Question3,
+         Question4,
+         Question5,
+         Question6,
+         Question7,
+         Question8,
+         Question9,
+         Question10
+       ) +
+       "%."
+   );
+
+   event.preventDefault();
+ });
+});
